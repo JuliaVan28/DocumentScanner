ddc0aed0a99b20749aa3147cf23708fb79a79b73
JuliaVan28 <julia.van2801@gmail.com>
2024-05-23 12:58:53 +0200

    Implemented DocumentScanner in SwiftUI

--- DocumentScanner/ContentView.swift
+++ DocumentScanner/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isScannerPresented: Bool = false
+    @State private var scannedImages: [UIImage] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if !scannedImages.isEmpty {
+                ScrollView {
+                    LazyVStack( content: {
+                        ForEach(scannedImages, id: \.self) { image in
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        }
+                    })
+                }
+            }
+            Button {
+                isScannerPresented.toggle()
+            } label: {
+                Text("Scan text")
+                    .font(.title3)
+                    .padding()
+            }.buttonStyle(.plain)
+                .background(.regularMaterial)
+                .clipShape(.capsule)
         }
-        .padding()
+        .sheet(isPresented: $isScannerPresented, content: {
+            ScannerView { result in
+                switch result {
+                case .success(let scannedImages):
+                    
+                    self.scannedImages = scannedImages
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                isScannerPresented = false
+            } didCancel: {
+                isScannerPresented = false
+            }
+        })
     }
 }
 
