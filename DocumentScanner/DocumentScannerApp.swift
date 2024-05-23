ca5d5eaabd0f201ad6c4db33565851e872f76fe2
JuliaVan28 <julia.van2801@gmail.com>
2024-05-23 09:01:41 +0200

    Initial Commit

---
+++ DocumentScanner/DocumentScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentScannerApp.swift
+//  DocumentScanner
+//
+//  Created by Yuliia on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DocumentScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
